e29f1e0b7930911daf264fb03e052efbce3d2e1b
Hayk <haykkyureghian@gmail.com>
2018-06-25 19:25:08 +0400

    first commit

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-let list = document.getElementById("myUL"),
+let list = document.getElementById("myOl"),
     input = document.getElementById('myInput'),
     listItem = null;
 
@@ -6,7 +6,6 @@ input.addEventListener("keyup",function () {
   for (let i = 0; i < list.children.length; i++) {
      listItem = list.children[i].textContent.toLowerCase();
     if (listItem.indexOf(input.value.toLowerCase()) === -1) {
-      console.log(input.value);
       list.children[i].style.display = "none";
     }else {
       list.children[i].style.display = "block";
